71606e23b45ee227f04747d0d3f9a1becb529e3e
dipti <dipti@DESKTOP-F1UMDA6.broadband>
2022-12-01 09:26:42 +0000

    Test Automation

---
+++ E2ESeleniumJenkins/src/testcases/Tc_3.java
@@ -0,0 +1,22 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Tc_3 {
+	
+	@Test
+	
+	public void testcase1() {
+		System.setProperty("webdriver.chrome.driver","./Driver/chromedriver12.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.findElementByXPath("//button[@title='Only allow essential cookies']").click();
+		driver.findElementById("email").sendKeys("Hello");
+		driver.findElementById("pass").sendKeys("Hello");
+		driver.quit();
+		
+	}
+	
+
+}
